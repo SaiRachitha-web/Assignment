2d71e82d2cda0905a23e438815d7178516e422ee
SaiRachitha-web <polurusairachitha@gmail.com>
2025-12-19 08:55:44 +0530

    Add files via upload

---
+++ TriangleWithSpaces.java
@@ -0,0 +1,21 @@
+public class TriangleWithSpaces {
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int s = rows; s > i; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1) || i == rows) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
