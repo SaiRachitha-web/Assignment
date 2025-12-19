e420a98f0bc6ad3236d7211d1b88901c3db07dc0
SaiRachitha-web <polurusairachitha@gmail.com>
2025-12-19 08:56:12 +0530

    Add files via upload

---
+++ TestAnimal.java
@@ -0,0 +1,17 @@
+class Animal{
+  void makeSound(){
+    System.out.println("animals are better than human beings");
+  }
+  }
+ class Dog extends Animal{
+ void makeSound(){
+    super.makeSound();
+     System.out.println("dog barks");
+ }
+}
+public class TestAnimal {
+    public static void main(String[] args) {
+        Dog d=new Dog();
+        d.makeSound();
+    }
+}
