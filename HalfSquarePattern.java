ff0cd0f0446949045dd65641b287e4af5fe1d6c3
SaiRachitha-web <polurusairachitha@gmail.com>
2025-12-19 08:57:57 +0530

    Add files via upload

---
+++ HalfSquarePattern.java
@@ -0,0 +1,18 @@
+public class HalfSquarePattern {
+    public static void main(String[] args) {
+
+        int rows = 4;
+        int cols = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (j == 1 || j == cols || i == rows) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
